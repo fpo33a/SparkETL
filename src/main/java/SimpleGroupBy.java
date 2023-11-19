10f088622973d8cdfc02fad57805e05cc20ab457
frank polet <frank.polet@gmail.com>
2023-11-19 11:40:02 +0100

    testing groupby/window adding agg column in repartition

--- src/main/java/SimpleGroupBy.java
+++ src/main/java/SimpleGroupBy.java
@@ -1,25 +1,17 @@
-/*
+// compare groupby aggregate & window functions
 
-Basic example of spark job doing group by operation
+/* executions time on home laptop
 
-This example requires to have some spark classes in class path
-  in intellij add project module dependencies to directory C:\frank\spark-2.3.0-bin-hadoop2.7\jars
+1/ with simple repartition (10) ( no column )
+- groupby : 51 sec
+- window  : 37 sec
 
-1/ Data to load
-C:\windows\system32>more c:\temp\data.csv
-id;parentid;data;date;url
-1;1;this is line 1;12/21/2021;www.line1.com
-2;1;this is line 2;12/22/2021;www.line2.com
-3;2;this is line 3;12/23/2021;www.line3.com
-4;2;this is line 4;12/24/2021;www.line4.com
+2/ with repartition on agg column (10, col("parentid")
+- groupby : 38 sec
+- window  : 8 sec
 
-2/ execution
-C:\frank\SparkLoadFile\target>java -cp "SparkLoadFile-1.0-SNAPSHOT-jar-with-dependencies.jar;C:\frank\spark-2.3.0-bin-hadoop2.7\jars\*" SparkGroupBy
+ */
 
-[...]
-*/
-
-import org.apache.spark.api.java.function.*;
 import org.apache.spark.sql.*;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.expressions.Window;
@@ -27,7 +19,6 @@ import org.apache.spark.sql.expressions.Window;
 import java.util.Date;
 
 import static org.apache.spark.sql.functions.col;
-import static org.apache.spark.sql.functions.window;
 
 
 public class SimpleGroupBy {
@@ -70,20 +61,20 @@ public class SimpleGroupBy {
         System.out.println("simpleGroupByKeyData ");
         System.out.println("----------------------------------------------------------");
         d0 = new Date();
-        this.simpleGroupByKeyData(extractedData).show();
+        this.simpleGroupByKeyData(extractedData);
         d1 = new Date();
 
         System.out.println("----------------------------------------------------------");
         System.out.println("windowGroupByKeyData ");
         System.out.println("----------------------------------------------------------");
         d2 = new Date();
-        this.windowGroupByKeyData(extractedData).show();
+        this.windowGroupByKeyData(extractedData);
         d3 = new Date();
 
         System.out.println("----------------------------------------------------------");
         long t1 = d1.getTime() - d0.getTime();
         long t2 = d3.getTime() - d2.getTime();
-        System.out.println("simpleGroupByKeyData = "+t1+", windowGroupByKeyData "+t2);
+        System.out.println("simpleGroupByKeyData = " + t1 + ", windowGroupByKeyData " + t2);
         System.out.println("----------------------------------------------------------");
 
         wait(600);
@@ -102,7 +93,7 @@ public class SimpleGroupBy {
                 .option("dateFormat", "M/d/y")
                 .option("inferSchema", true)
                 .load(filename)
-                .repartition(10);
+                .repartition(10, col("parentid"));
 
         System.out.println("Schema:");
         df.printSchema();
@@ -117,17 +108,17 @@ public class SimpleGroupBy {
     Dataset<Row> simpleGroupByKeyData(Dataset<Row> dataset) {
 
         System.out.println("----------------------------------------------------------");
-        System.out.println("simpleGroupByKeyData starting "+new java.util.Date());
+        System.out.println("simpleGroupByKeyData starting " + new java.util.Date());
         System.out.println("----------------------------------------------------------");
 
 
-        Dataset<Row> gbDs = dataset.groupBy(col("parentid")).count().withColumnRenamed("parentid", "pid") ;
-        Dataset<Row> result = dataset.join( gbDs, dataset.col( "parentId" ).equalTo(gbDs.col("pid")), "inner"  ).drop("pid");
-        result.write().option("header",true).mode("overwrite").csv("c:/temp/simple.csv");
+        Dataset<Row> gbDs = dataset.groupBy(col("parentid")).count().withColumnRenamed("parentid", "pid");
+        Dataset<Row> result = dataset.join(gbDs, dataset.col("parentId").equalTo(gbDs.col("pid")), "inner").drop("pid");
+        result.write().option("header", true).mode("overwrite").csv("c:/temp/simple.csv");
 
 
         System.out.println("----------------------------------------------------------");
-        System.out.println("simpleGroupByKeyData ending "+new java.util.Date());
+        System.out.println("simpleGroupByKeyData ending " + new java.util.Date());
         System.out.println("----------------------------------------------------------");
         return result;
 
@@ -138,16 +129,16 @@ public class SimpleGroupBy {
     Dataset<Row> windowGroupByKeyData(Dataset<Row> dataset) {
 
         System.out.println("----------------------------------------------------------");
-        System.out.println("windowGroupByKeyData starting "+new java.util.Date());
+        System.out.println("windowGroupByKeyData starting " + new java.util.Date());
         System.out.println("----------------------------------------------------------");
 
 
         Dataset<Row> result = dataset.withColumn("count", functions.count("*").over(Window.partitionBy("parentid")));
-        result.write().option("header",true).mode("overwrite").csv("c:/temp/window.csv");
+        result.write().option("header", true).mode("overwrite").csv("c:/temp/window.csv");
 
 
         System.out.println("----------------------------------------------------------");
-        System.out.println("windowGroupByKeyData ending "+new java.util.Date());
+        System.out.println("windowGroupByKeyData ending " + new java.util.Date());
         System.out.println("----------------------------------------------------------");
         return result;
 
